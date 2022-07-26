9ad45000b2eba8f81442b6d8b97923eb3f200516
malouda0000 <hamam.hamza85@gmail.com>
2022-07-26 15:41:53 -0700

    adding featchers to Setting screen

--- lib/screens/setting screen/setting_screen.dart
+++ lib/screens/setting screen/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_demo/constants.dart';
 import 'package:my_demo/widgets/bottom_nav_bar.dart';
 import 'package:my_demo/screens/home/components/the_app_bar.dart';
 import 'package:my_demo/screens/home/components/the_body.dart';
@@ -19,36 +20,65 @@ class SettingScreen extends StatelessWidget {
       drawer: TheDrawer(),
       appBar: TheAppBar(),
       // drawer: (),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            emptySpace,
-            Container(
-              height: MediaQuery.of(context).size.width * 0.3,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/me.jpg'),
-                  fit: BoxFit.fitHeight,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              emptySpace,
+              Container(
+                height: MediaQuery.of(context).size.width * 0.3,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/me.jpg'),
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
+                // child: Image.asset(
+                //   'assets/images/me.jpg',
+                //   // height: 250,
+                //   // width: 250,
+                // ),
               ),
-              // child: Image.asset(
-              //   'assets/images/me.jpg',
-              //   // height: 250,
-              //   // width: 250,
-              // ),
-            ),
-            emptySpace,
-            Text(
-              'hummam mohamed '.toUpperCase(),
-            ),
-          ],
+              emptySpace,
+              Text(
+                'hummam mohamed '.toUpperCase(),
+              ),
+              Divider(
+                color: ksecondaryColor,
+                thickness: 2,
+              ),
+              SettingItem(),
+              SettingItem(),
+              SettingItem(),
+              SettingItem(),
+              SettingItem(),
+            ],
+          ),
         ),
       ),
       floatingActionButton: TheFap(),
       bottomNavigationBar: TheBottomeNavBar(),
+    );
+  }
+}
+
+class SettingItem extends StatelessWidget {
+  SettingItem({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    String title = "the new method";
+    String discription = 'theee discription';
+    return ListTile(
+      leading: const Icon(Icons.home),
+      trailing: const Icon(Icons.send),
+      title: Text(title),
+      subtitle: Text(discription),
+      onTap: () {},
     );
   }
 }
